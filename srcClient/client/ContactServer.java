1855ba01b86bc3709eeabd5b195305ec6eb05b63
Alexandre Marotti da Fonseca Temporal <pvp_marotti2hotmail.com.br>
2019-06-29 18:03:58 -0300

    changes

--- srcClient/client/ContactServer.java
+++ srcClient/client/ContactServer.java
@@ -1,20 +1,26 @@
 package client;
 import util.Messages;
 import java.lang.Thread;
-
+import java.net.URLEncoder;
+import java.net.URLDecoder;
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.OutputStream;
 import java.net.InetAddress;
 import java.net.Socket;
+
 import java.net.UnknownHostException;
 import java.util.LinkedList;
+
 
 public class ContactServer extends Thread{
 	Socket server;
 	byte[] sendData, receivedData;
-	LinkedList l;
+	LinkedList send, recieve;
 	
-	public ContactServer(LinkedList lTemp) {
+	public ContactServer(LinkedList lSend, LinkedList lRecieve) {
 		/*int port = 5093;
 		InetAddress IPserver;
 		try {
@@ -25,35 +31,56 @@ public class ContactServer extends Thread{
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}*/
-		l = lTemp;
+		send = lSend;
+		recieve = lRecieve;
 		
 	}
 	
 	private void enviarMensagem(OutputStream os) throws Exception {
 		
-		String s = (String)l.pop();
+		String s = (String)send.pop();
 		byte[] saida = s.getBytes();
 		
 		os.write(saida);
 		os.flush();
 	}
 	
-	private void receberMensagem(LinkedList<Messages> message) {
+	private void receberMensagem(InputStream is) throws IOException {
 		// TODO fazer metodo
+		
+		is.read(receivedData);
+		
 	}
 	
 	@Override
 	public void run() {
 		// TODO Auto-generated method stub
 		while(true) {
-			OutputStream os = server.getOutputStream();
-			if(!l.isEmpty()) {
-				System.out.println("not empty");
-				l.pop();
-			}
 			try {
+				//-----------------------inicio do try---------------------------------------
+				OutputStream os = server.getOutputStream();
+				InputStream is = server.getInputStream();
+				BufferedOutputStream bos = new BufferedOutputStream(os);
+				BufferedInputStream bis = new BufferedInputStream(is);
+				
+				enviarMensagem(os);
+				receberMensagem(is);
+				
+				URLEncoder.encode("", "UTF-8");
+				URLDecoder.decode("", "UTF-8");
+				
 				Thread.sleep(300);
+				
+				
+				
+				//-----------------------fim do try--------------------------------
 			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (Exception e) {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
